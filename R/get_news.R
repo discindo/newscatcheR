b1ad99af9b2e7202bdf36c2e3a29cc42e314db53
teofiln <teofiln@gmail.com>
2020-04-25 07:45:15 -0600

    drop pipe and dplyr. use explicit stop for errors. update documentation

--- R/get_news.R
+++ R/get_news.R
@@ -3,34 +3,26 @@
 #' @param website a url of a new source in the format "news.ycombinator.com"
 #'
 #' @return a tibble containing the contents of the rss feed
-#' @import tidyverse
 #' @import tidyRSS
 #' @export
 #' @examples
-#' datapath <- system.file("example-data/my-dataset.csv", package = "newscatcheR")
-#' example_data <- readr::read_csv(datapath, col_names = c("url", "rss"))
-#' get_news("news.ycombinator.com")
+#' get_news(website = "theguardian.com")
 
 get_news <- function(website = "news.ycombinator.com") {
 
   # check if argument is character
   if (is.character(website) == FALSE) {
-    print("Website should be character vector.")
+    stop("Website should be character vector.")
 
     # check if it exists in dataframe and inform if it doesn't
-  } else if (is.element(website, rss_table$url) == FALSE) {
-    print("We don't have that news source.")
+  } else if (!all(website %in% rss_table$url)) {
+    faulty <- website[!website %in% rss_table$url]
+    stop(paste("Can't find website(s):", faulty, "in our database. Please check if these are valid website names.", sep=" "))
 
     # return if it does
   } else {
-    news_source <- rss_table %>%
-      filter(url == website) %>%
-      pull(rss_endpoint)
-
+    news_source <- rss_table$rss_endpoint[rss_table$url %in% website]
     feed_entries <- tidyfeed(news_source) # this fails on some feeds
-
   }
-
   return(feed_entries)
-
 }
