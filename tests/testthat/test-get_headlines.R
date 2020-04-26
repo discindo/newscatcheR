09036bb3f9be1b552f9aaee8d82de2228cdb6a8c
novica <nnovica@gmail.com>
2020-04-26 19:32:18 +0200

    add more tests

--- tests/testthat/test-get_headlines.R
+++ tests/testthat/test-get_headlines.R
@@ -1,12 +1,12 @@
-test_that("get_news works", {
+test_that("get_headlines works", {
   # adding a small time delay to avoid simultaneous posts to the api
   Sys.sleep(1)
 
   # Get the news
-  output_news <- get_news("news.ycombinator.com")
+  output_headlines <- get_headlines("news.ycombinator.com")
 
   # What's returned
-  expect_is(output_news, "data.frame")
+  expect_is(output_headlines, "data.frame")
+  expect_equal(ncol(output_headlines), 1)
 
 })
-
--- tests/testthat/test-get_news.R
+++ tests/testthat/test-get_news.R
@@ -1,12 +1,24 @@
-test_that("get_headlines works", {
+test_that("get_news works", {
   # adding a small time delay to avoid simultaneous posts to the api
   Sys.sleep(1)
 
   # Get the news
-  output_headlines <- get_headlines("news.ycombinator.com")
+  output_news <- get_news("news.ycombinator.com")
 
   # What's returned
-  expect_is(output_headlines, "data.frame")
-  expect_equal(ncol(output_headlines), 1)
+  expect_is(output_news, "data.frame")
+
+  # adding a small time delay to avoid simultaneous posts to the api
+  Sys.sleep(1)
+
+  # numbers return an error
+  expect_error(output_news <- get_news(123))
+
+  # adding a small time delay to avoid simultaneous posts to the api
+  Sys.sleep(1)
+
+  # url not in database returns error
+  expect_error(output_news <- get_news("example.com"))
 
 })
+
