d45be4c6ec293ff7213b0bb90ae7971f903dc7a7
novica <nnovica@gmail.com>
2020-06-05 13:23:52 +0200

    last check successful

--- dev_history.R
+++ dev_history.R
@@ -15,19 +15,18 @@ attachment::att_to_description()
 # Prepare raw data for rda.
 
 library(tidyverse)
-library(readr)
 
 
-rss_table_raw <- read_delim("inst/external-data/rss_table.csv",
+rss_table_raw <- readr::read_delim("inst/external-data/rss_table.csv",
                             "|", escape_double = FALSE, trim_ws = TRUE)
 
 # one url has non ascii characters so make sure we use escaped
 hakolhayehudi <- "https://www.hakolhayehudi.co.il/rss/%D7%AA%D7%A8%D7%95%D7%9D_%D7%9C%D7%A0%D7%95"
 
 
-rss_table <- rss_table %>%
+rss_table <- rss_table_raw %>%
   mutate(rss_endpoint = if_else(str_detect(rss_endpoint,
                                            "hakolhayehudi"), hakolhayehudi, rss_endpoint))
 
 
-usethis::use_data(rss_table)
+usethis::use_data(rss_table, overwrite = TRUE)
