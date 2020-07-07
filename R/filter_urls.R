86c468734da7f2da7ede72ec60dc107f6a195ea5
novica <nnovica@gmail.com>
2020-07-07 20:23:34 +0200

    add examples

--- R/filter_urls.R
+++ R/filter_urls.R
@@ -2,15 +2,18 @@
 #' FIlter URLS in the provided database.
 #'
 #' @param topic the topic of the feed see `show_topics()` for more.
+#' @param country the country of origin of the feed using two capital
+#' #letters, for example "US". See `show_countries()` for more.
+#' @param language the language of the content of the feed using two
+#' #lowercase letters, for example "en". See `show_languages()` for more.
 #' @param rss_table a dataframe with urls and rss feeds in case you
 #' #need to construct your own out of webisites not in the included database.
-#' @param country the country of the feed see `show_countries()` for more.
-#' @param language the language of the feed see `show_languages()` for more.
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' filter_urls(topic = "tech", country = "US", language =)
 filter_urls <- function(topic = NULL, country = NULL, language = NULL, rss_table = package_rss) {
 
   if (!is.null(topic)) {
