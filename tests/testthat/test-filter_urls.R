ef09a30e52fffdf8395eb6ce3a2f27b7fe6cff2f
teofiln <teofiln@gmail.com>
2020-07-11 10:30:52 -0600

    add tests

--- tests/testthat/test-filter_urls.R
+++ tests/testthat/test-filter_urls.R
@@ -31,10 +31,27 @@ test_that("filter_urls filters the feeds dataset", {
   expect_s3_class(output_filter, "data.frame")
 
   # And check the errors
-  expect_message(output_filter = filter_urls(topic = "mytopic"))
+  expect_null(filter_urls(topic = "mytopic"))
+  expect_null(filter_urls(country = "gondor"))
+  expect_null(filter_urls(language = "klingon"))
 
-  expect_message(output_filter = filter_urls(topic = "tech", country = "XX"))
+  expect_null(filter_urls(topic = "tech", country = "XX"))
+  expect_null(filter_urls(topic = "tech", language = "romulan"))
+  expect_null(filter_urls(country = "tech", topic = "XX"))
 
-  expect_error(output_filter = filter_urls(topic = 123))
+  expect_null(filter_urls(topic = "tech", country = "XX", language = "de"))
+  expect_null(filter_urls(topic = "tech", language = "romulan", country = "US"))
+  expect_null(filter_urls(topic = "musi", country = "US", language = "en"))
+
+  expect_error(filter_urls(topic = 1, country = "XX"))
+  expect_error(filter_urls(topic = list("tech"), language = "romulan"))
+  expect_error(filter_urls(topic = "tech", country = 22))
+
+  expect_error(filter_urls(topic = NA))
+  expect_error(filter_urls(country = 123))
+  expect_error(filter_urls(language = tibble("lang"="klingon")))
+
+
+  expect_error(filter_urls(topic = 123))
 
 })
