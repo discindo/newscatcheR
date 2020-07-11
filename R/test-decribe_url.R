b34e41ad734803b5fc973ca064eda5f81f8cd7a2
novica <nnovica@gmail.com>
2020-07-11 17:01:02 +0200

    add tests

---
+++ R/test-decribe_url.R
@@ -0,0 +1,9 @@
+test_that("describe_url returns a message with topic", {
+
+  # Get the news
+  output_describe <- describe_url("ycombinator.com")
+
+  # What's returned
+  expect_type(output_headlines, "character")
+
+})
---
+++ tests/testthat/test-filter_urls.R
@@ -0,0 +1,40 @@
+test_that("filter_urls filters the feeds dataset", {
+
+  # Filter topic
+  output_filter <- filter_urls(topic = "tech")
+
+  # What's returned
+  expect_is(output_filter, "data.frame")
+
+  # Filter country
+  output_filter <- filter_urls(country = "US")
+
+  # What's returned
+  expect_is(output_filter, "data.frame")
+
+  # Filter languge
+  output_filter <- filter_urls(language = "en")
+
+  # What's returned
+  expect_is(output_filter, "data.frame")
+
+  # Works with two arguments
+  output_filter <- filter_urls(topic = "tech", country = "US")
+
+  # What's returned
+  expect_is(output_filter, "data.frame")
+
+  # Works with three arguments
+  output_filter <- filter_urls(topic = "tech", country = "US", language = "en")
+
+  # What's returned
+  expect_is(output_filter, "data.frame")
+
+  # And check the errors
+  expect_error(output_filter = filter_urls(topic = "mytopic"))
+
+  expect_error(output_filter = filter_urls(topic = "tech", country = "XX"))
+
+  expect_error(output_filter = filter_urls(topic = 123))
+
+})
