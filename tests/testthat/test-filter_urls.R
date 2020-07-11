287c7f35f4db5b6c4f94510038a0d67064be6f92
novica <nnovica@gmail.com>
2020-07-11 17:28:23 +0200

    update tests

--- tests/testthat/test-filter_urls.R
+++ tests/testthat/test-filter_urls.R
@@ -31,9 +31,9 @@ test_that("filter_urls filters the feeds dataset", {
   expect_s3_class(output_filter, "data.frame")
 
   # And check the errors
-  expect_error(output_filter = filter_urls(topic = "mytopic"))
+  expect_message(output_filter = filter_urls(topic = "mytopic"))
 
-  expect_error(output_filter = filter_urls(topic = "tech", country = "XX"))
+  expect_message(output_filter = filter_urls(topic = "tech", country = "XX"))
 
   expect_error(output_filter = filter_urls(topic = 123))
 
