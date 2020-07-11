6c523b2cf0e62d043887f76513aaf8302d70dfb6
teofiln <teofiln@gmail.com>
2020-07-11 10:30:29 -0600

    capture different types of missingness. print nice messages. return invisible.

--- R/filter_urls.R
+++ R/filter_urls.R
@@ -14,33 +14,69 @@
 #'
 #' @examples
 #' filter_urls(topic = "tech", country = "US", language = "en")
-filter_urls <- function(topic = NULL, country = NULL, language = NULL, rss_table = package_rss) {
+filter_urls <-
+  function(topic = NULL,
+           country = NULL,
+           language = NULL,
+           rss_table = package_rss) {
+    work_table <- rss_table
 
-  if (!is.null(topic)) {
-    stopifnot("'topic' should be character string." = is.character(topic))
-    rss_table <- rss_table[rss_table$topic_unified == topic, ]
-    if (nrow(rss_table) == 0) {
-      message("Can't find the requested topic in our database. Choose some of the available topics: ",
-              paste0(show_topics(), collapse = ", "), ".")
+    if (!is.null(topic)) {
+      stopifnot("'topic' should be character string." = is.character(topic))
+      work_table <- work_table[work_table$topic_unified == topic,]
+      if (nrow(work_table) == 0) {
+        if (topic %in% rss_table$topic_unified) {
+          message(
+            "The topic is in the database, but has no entries in combination with the other filters."
+          )
+        } else {
+          message(
+            "Can't find the requested topic in our database. Choose some of the available topics: ",
+            paste0(show_topics(), collapse = ", "),
+            "."
+          )
+        }
+        return(invisible(NULL))
       }
-  }
+    }
 
-  if (!is.null(country)) {
-    stopifnot("'country' should be character string." = is.character(country))
-    rss_table <- rss_table[rss_table$clean_country == country, ]
-    if (nrow(rss_table) == 0) {
-      message("Can't find the requested country in our database. Choose some of the available countries: ",
-              paste0(show_countries(), collapse = ", "), ".")
+    if (!is.null(country)) {
+      stopifnot("'country' should be character string." = is.character(country))
+      work_table <- work_table[work_table$clean_country == country, ]
+      if (nrow(work_table) == 0) {
+        if (country %in% rss_table$clean_country) {
+          message(
+            "The country is in the database, but has no entries in combination with the other filters."
+          )
+        } else {
+          message(
+            "Can't find the requested country in our database. Choose some of the available countries: ",
+            paste0(show_countries(), collapse = ", "),
+            "."
+          )
+        }
+        return(invisible(NULL))
       }
-  }
+    }
 
-  if (!is.null(language)) {
-    stopifnot("'language' should be character string." = is.character(language))
-    rss_table <- rss_table[rss_table$language == language, ]
-    if (nrow(rss_table) == 0) {
-      message("Can't find the requested language in our database. Choose some of the available languages: ",
-              paste0(show_language(), collapse = ", "), ".")
+    if (!is.null(language)) {
+      stopifnot("'language' should be character string." = is.character(language))
+      work_table <- work_table[work_table$language == language, ]
+      if (nrow(work_table) == 0) {
+        if (language %in% rss_table$language) {
+          message(
+            "The language is in the database, but has no entries in combination with the other filters."
+          )
+        } else {
+          message(
+            "Can't find the requested language in our database. Choose some of the available languages: ",
+            paste0(show_languages(), collapse = ", "),
+            "."
+          )
+        }
+        return(invisible(NULL))
       }
+    }
+
+    return(rss_table)
   }
-  return(rss_table)
-}
