9dc7f6a9609494b60f13cdda03105d86ee1a066c
teofiln <teofiln@gmail.com>
2020-07-11 00:21:12 -0600

    simplified logic for filtering. typo in documentation

--- R/filter_urls.R
+++ R/filter_urls.R
@@ -7,7 +7,7 @@
 #' @param language the language of the content of the feed using two
 #' #lowercase letters, for example "en". See `show_languages()` for more.
 #' @param rss_table a dataframe with urls and rss feeds in case you
-#' #need to construct your own out of webisites not in the included database.
+#' #need to construct your own out of websites not in the included database.
 #'
 #' @return a tibble filtered according to the given parameters
 #' @export
@@ -16,52 +16,23 @@
 #' filter_urls(topic = "tech", country = "US", language = "en")
 filter_urls <- function(topic = NULL, country = NULL, language = NULL, rss_table = package_rss) {
 
-  if (!is.character(topic)) {
-    stop("'topic' should be character string.")
-  } else if (!any(show_topics() == topic) && !is.null(topic)) {
-    stop(
-      paste(
-        "Can't find topic:",
-        topic,
-        "in our database. See available topic with `show_topics()`.",
-        sep = " "
-      )
-    )
-  } else {
-    rss_table <- rss_table[rss_table$topic_unified == topic,]
+  if (!is.null(topic)) {
+    stopifnot("'topic' should be character string." = is.character(topic))
+    rss_table <- rss_table[rss_table$topic_unified == topic, ]
+    stopifnot("Can't find topic the requested topic in our database. See available topics with `show_topics()`." = {nrow(rss_table) > 0})
   }
 
-  if (!is.character(country)) {
-    stop("'country' should be character string.")
-  } else if (!any(show_countries() == country) && !is.null(country)) {
-    stop(
-      paste(
-        "Can't find country:",
-        country,
-        "in our database. See available countries with `show_countries()`.",
-        sep = " "
-      )
-    )
-  } else {
-    rss_table <- rss_table[rss_table$clean_country == country,]
+  if (!is.null(country)) {
+    stopifnot("'country' should be character string." = is.character(country))
+    rss_table <- rss_table[rss_table$clean_country == country, ]
+    stopifnot("Can't find country the requested country in our database. See available countries with `show_countries()`." = {nrow(rss_table) > 0})
   }
 
-
-  if (!is.character(language)) {
-    stop("'language' should be character string.")
-  } else if (!any(show_languages() == language) && !is.null(language)) {
-    stop(
-      paste(
-        "Can't find language:",
-        language,
-        "in our database. See availale languages with `show_languages()`.",
-        sep = " "
-      )
-    )
-  } else {
-    rss_table <- rss_table[rss_table$clean_country == country,]
+  if (!is.null(language)) {
+    stopifnot("'language' should be character string." = is.character(language))
+    rss_table <- rss_table[rss_table$language == language, ]
+    stopifnot(
+      "Can't find the requested language in our database. See available countries with `show_languages()`." = {nrow(rss_table) > 0 })
   }
-
   return(rss_table)
-
 }
