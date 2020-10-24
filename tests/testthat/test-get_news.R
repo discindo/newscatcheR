53e29abd3d08b471cba0527474ff6ed627f5008d
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2020-10-23 21:14:54 -0300

    Skipping tests if no internet connection

--- tests/testthat/test-get_news.R
+++ tests/testthat/test-get_news.R
@@ -2,11 +2,17 @@ test_that("get_news returns a tibble with feed contents", {
   # adding a small time delay to avoid simultaneous posts to the api
   Sys.sleep(3)
 
+  # Skip if the website could not be loaded
+  skip_if_offline("ycombinator.com")
+
   # Get the news
   output_news <- get_news("ycombinator.com")
 
   # What's returned
   expect_s3_class(output_news, "data.frame")
+
+  # Skip if the website could not be loaded
+  skip_if_offline("bbc.com")
 
   # Get the news
   output_news <- get_news("bbc.com", topic = "travel")
